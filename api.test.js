0da6d49225b69013babe3f6c66fa42909cc943af
EmileFrancois <emile.francois@bukalapak.com>
2022-04-09 14:53:29 +0700

    training ke2 crud

--- api.test.js
+++ api.test.js
@@ -9,3 +9,60 @@ describe('Todos endpoint', () => {
 		expect(response.statusCode).toBe(200);
 	});
 });
+
+describe('Post', () => {
+	it('should return a 201 status code', async () => {
+		const response = await request(baseUrl).post('/users')
+		.send({
+    "name": "morpheus",
+    "job": "leader"
+  })
+  .set('Accept', 'application/json')
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(201);
+	});
+});
+
+describe('Update', () => {
+	it('should return a 200 status code', async () => {
+		const response = await request(baseUrl).put('/users/2')
+		.send({
+    "name": "Joko",
+    "job": "Babu"
+  })
+  .set('Accept', 'application/json')
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(200);
+	});
+});
+
+describe('PATCH', () => {
+	it('should return a 200 status code', async () => {
+		const response = await request(baseUrl).patch('/users/2')
+		.send({
+    "name": "morpheus",
+    "job": "Lead"
+  })
+  .set('Accept', 'application/json')
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(200);
+	});
+});
+
+describe('DELETE', () => {
+	it('should return a 200 status code', async () => {
+		const response = await request(baseUrl).delete('/users/2')
+		.send({
+    "name": "morpheus",
+    "job": "Lead"
+  })
+  .set('Accept', 'application/json')
+
+    console.log(response.body)
+		expect(response.statusCode).toBe(204);
+		expect()
+	});
+});
